314aa140d59833b4d5d6beb1dfce0ae6930c5698
9847530565 <vaibhav.18bit1035@abes.ac.in>
2020-08-07 21:44:54 +0530

    application code

--- e mail sender.py
+++ e mail sender.py
@@ -3,7 +3,12 @@ from tkinter.filedialog import askopenfile
 from tkinter import simpledialog
 import smtplib
 import pandas as pd
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 screen=Tk()
+attachoption=False
 width,height=500,500
 screen.geometry(f"{width}x{height}")
 photo=PhotoImage(file="files//Settings.png")
@@ -19,6 +24,33 @@ def openfile():
     file=askopenfile(mode="r",filetypes=[("Excel Work Sheets","*.xlsx")])
     if file is not None:
         identity.set(file.name)
+def addfiles():
+    global attachoption
+    attachoption=True
+    filename=askopenfile(mode="a",filetypes=[("Excel Work Sheets",".xlsx"),("PDF Files","*.pdf"),("PNG image","*.png"),("JPEG image","*.jpeg"),("JPG images","*.jpg")])
+    x=filename.name
+    filename=open(x,"rb")
+    global msg
+    msg=MIMEMultipart()
+    msg["From"]=usermail
+    msg["To"]=identity.get()
+    body=box2.get("1.0","end")
+    box2.delete("1.0","end")
+    msg["Subject"]="E mail sending application"
+    msg.attach(MIMEText(body, 'plain'))
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((filename).read())
+    encoders.encode_base64(p)
+    #msg.attach(MIMEText(box2.get("1.0","end"), 'plain'))
+    p.add_header('Content-Disposition', "attachment; filename= %s" % x)
+    msg.attach(p)
+    if filename is not None:
+        #box2.delete("1.0","end")
+        box2.insert("1.0",x)
+def resetentries():
+    option.set(0)
+    identity.set("")
+    box2.delete("1.0","end")
 def sendemail():
     id1=identity.get()
     message=box2.get("1.0","end")
@@ -29,13 +61,14 @@ def sendemail():
         server.starttls()
         server.login(usermail,userpassword)
         if mailtype==1:
-            server.sendmail(usermail,id1,message)
+            server.sendmail(usermail,id1,msg.as_string())
+            atttachoption=False
         elif mailtype==2:
             id1.replace('/', "\\")
             df=pd.read_excel(id1)
             df1=df["email"]
             for i in df1:
-                server.sendmail("vs9837194@gmail.com",i,message)
+                server.sendmail(usermail,i,msg.as_string())
         server.quit()
     else:
         messagebox.showerror("Fatal Error","Please enter email address")
@@ -50,11 +83,12 @@ global identity,box2
 identity=StringVar()
 Entry(screen,text=identity,width=30,bg="red",fg="cyan").place(relx=0.25,x=-2,y=2*height//5)
 Button(screen,text="browse",command=openfile).place(relx=0.7,x=-2,y=2*height//5)
+Button(screen,text="add file",command=addfiles).place(relx=0.7,x=-2,y=3*height//5)
 Label(screen,text="enter content",bg="green",font=("Calibri",10),fg="cyan").place(relx=0,x=-2,y=height//2)
 box2=Text(screen,height=10,width=30,bg="red",fg="cyan",font=("Calibri",10))
 box2.place(relx=0.25,x=-2,y=height//2)
-Button(screen,text="Send",command=sendemail).place(relx=0.5,x=-2,y=4.5*height//5)    #df=pd.read_csv("C:\\Users\\hp\\Downloads\\abc.csv")
-#print(df.head())
+Button(screen,text="Send",command=sendemail).place(relx=0.5,x=-2,y=4.5*height//5)
+Button(screen,text="reset",command=resetentries).place(relx=0.7,x=-2,y=4.5*height//5)
 global username,userpassword
 usermail=simpledialog.askstring("input","enter your unofficial email id",parent=screen)
 userpassword=simpledialog.askstring("input","enter your password",parent=screen)
